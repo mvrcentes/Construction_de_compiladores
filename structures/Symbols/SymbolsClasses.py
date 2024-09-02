c215cc4ee5c0c8bb43a5561435341530ecff7d34
mvrcentes <mvrcentes@hotmail.com>
2024-09-02 08:31:13 -0600

    Add: add Variable class

--- structures/Symbols/SymbolsClasses.py
+++ structures/Symbols/SymbolsClasses.py
@@ -17,3 +17,19 @@ class Constant(Symbol):
 
     def __repr__(self):
         return f"Constant (type={self.type}, value={self.value})"
+    
+class Variable(Symbol):
+    def __init__(self, name: str, type: Type = NilType(), value: any = None):
+        super().__init__(name, type)
+        self.value = value
+
+    # Set type
+    def set_type(self, type: Type):
+        self.type = type
+
+    # Set new value
+    def set_value(self, value: any):
+        self.value = value
+
+    def __repr__(self):
+        return f"Variable (type={self.type}, value={self.value})"
