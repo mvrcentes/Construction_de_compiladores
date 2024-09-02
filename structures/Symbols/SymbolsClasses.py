652b554c3062cdce77652168c88bc5a86d5327db
mvrcentes <mvrcentes@hotmail.com>
2024-09-02 08:31:48 -0600

    Add: add Parameter and Function class

--- structures/Symbols/SymbolsClasses.py
+++ structures/Symbols/SymbolsClasses.py
@@ -33,3 +33,31 @@ class Variable(Symbol):
 
     def __repr__(self):
         return f"Variable (type={self.type}, value={self.value})"
+
+class Parameter(Symbol):
+    def __init__(self, name: str, ctx:any, type: Type = NilType(), value: any = None):
+        super().__init__(name, ctx, type)
+        self.value = value
+
+    # Set type
+    def set_type(self, type: Type):
+        self.type = type
+
+    # Set value
+    def set_value(self, value: any):
+        self.value = value
+
+    def __repr__(self):
+        return f"Parameter (type={self.type}, value={self.value})"
+
+class Function(Symbol):
+    def __init__(self, name: str, ctx: any, return_type: Type = NilType(), parameters: list = None):
+        super().__init__(name, ctx, return_type)
+        self.parameters = parameters if parameters is not None else []
+
+    def add_parameter(self, parameter: Parameter):
+        self.parameters.append(parameter)
+
+    # Set the return type of the function until it is known
+    def set_return_type(self, return_type: Type):
+        self.type = return_type
