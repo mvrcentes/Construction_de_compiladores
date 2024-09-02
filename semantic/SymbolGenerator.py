2ddda97e4a0f3d0c37a7a8825c38470fbb9c31b1
mvrcentes <mvrcentes@hotmail.com>
2024-09-01 19:08:26 -0600

    Add: add visit functions

--- semantic/SymbolGenerator.py
+++ semantic/SymbolGenerator.py
@@ -4,6 +4,10 @@ from antlr4 import *
 
 from lexer_parser.CompiScriptLanguageVisitor import CompiScriptLanguageVisitor
 from lexer_parser.CompiScriptLanguageParser import CompiScriptLanguageParser
+from structures.Types.TypesClasses import *
+from structures.Types.TypesTable import TypesTable
+from structures.Symbols.SymbolsClasses import *
+from structures.ContextManagement import *
 
 class SymbolGenerator(CompiScriptLanguageVisitor):
     def __init__(self):
@@ -30,3 +34,32 @@ class SymbolGenerator(CompiScriptLanguageVisitor):
         self.context_manager.current_context.symbol_table.print_table()
 
         return result
+    
+    # Visit a parse tree produced by CompiScriptLanguageParser#classDeclaration.
+    def visitClassDeclaration(self, ctx:CompiScriptLanguageParser.ClassDeclarationContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by CompiScriptLanguageParser#functionDeclaration.
+    def visitFunctionDeclaration(self, ctx:CompiScriptLanguageParser.FunctionDeclarationContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by CompiScriptLanguageParser#variableDeclaration.
+    def visitVariableDeclaration(self, ctx:CompiScriptLanguageParser.VariableDeclarationContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by CompiScriptLanguageParser#statementDeclaration.
+    def visitStatementDeclaration(self, ctx:CompiScriptLanguageParser.StatementDeclarationContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by CompiScriptLanguageParser#classDecl.
+    def visitClassDecl(self, ctx:CompiScriptLanguageParser.ClassDeclContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by CompiScriptLanguageParser#funDecl.
+    def visitFunDecl(self, ctx:CompiScriptLanguageParser.FunDeclContext):
+        return self.visitChildren(ctx)
