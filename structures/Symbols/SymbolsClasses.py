d9fffd33594fc8f691dee3eb4abd86b4e3f148d1
mvrcentes <mvrcentes@hotmail.com>
2024-09-02 08:30:38 -0600

    Add: add Constant class

--- structures/Symbols/SymbolsClasses.py
+++ structures/Symbols/SymbolsClasses.py
@@ -7,3 +7,13 @@ class Symbol:
 
     def __repr__(self):
         return f"Symbol (type={self.type})"
+
+class Constant(Symbol):
+    def __init__(self, name: str, ctx: any, type: Type, value: any):
+        if value is None or type is None:
+            raise ValueError("A constant must have an initial type and value")
+        super().__init__(name, ctx, type)
+        self.value = value
+
+    def __repr__(self):
+        return f"Constant (type={self.type}, value={self.value})"
